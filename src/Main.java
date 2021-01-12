6b11870178fe2d4a64b3c3d25905185052e79a31
isaccanedo <isaccanedo@gmail.com>
2021-01-12 10:59:46 -0300

    :lipstick: Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+	
+	/*
+	 * What is the output of this code?
+	 * (Qual é a saída deste código?)
+	 * 
+	 * 1 - ello ==> correct
+	 * 2 - The code does not compile
+	 * 3 - Hello
+	 * 4 - A runtime exceptions is thrown
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		
+		String message = "Hello";
+		for (int i = 0; i < message.length(); i++) {
+			
+			System.out.println(message.charAt(i + 1));
+			
+		}		
+		
+	}
+
+}
